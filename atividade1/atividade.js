65a5570a72e2aa546e541cf7740994fc5973674d
ANDRÉ LUIZ SELESTINO MARCELINO <andre.marcelino@a.ucb.br>
2024-08-16 10:47:34 -0300

    atividade

---
+++ atividade1/atividade.js
@@ -0,0 +1,12 @@
+let num1 = 6;
+let num2 = 3;
+
+let soma =  num1 + num2;
+let subt = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
+
+console.log(`O resultado da soma de entre ${num1} e ${num2} é ${soma}`);
+console.log(`O resultado da subtração de entre ${num1} e ${num2} é ${subt}`);
+console.log(`O resultado da multiplicação de entre ${num1} e ${num2} é ${mult}`);
+console.log(`O resultado da divisão de entre ${num1} e ${num2} é ${div}`);
