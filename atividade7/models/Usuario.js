79ac856348f6fa81c2019aa1834d79e09058da36
Andre <andreluizselestino@gmail.com>
2024-11-21 21:50:47 -0300

    atividade 7

---
+++ atividade7/models/Usuario.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Usuario = db.define('usuario', {
+    id: { 
+        type: Sequelize.INTEGER, 
+        autoIncrement: true, 
+        primaryKey: true 
+    },
+    nome: {
+         type: Sequelize.STRING,
+          allowNull: false 
+    },
+    email: {
+         type: Sequelize.STRING,
+          allowNull: false,
+           unique: true 
+    },
+    senha: {
+        type: Sequelize.STRING,
+         allowNull: false 
+    }
+});
+
+module.exports = Usuario;
